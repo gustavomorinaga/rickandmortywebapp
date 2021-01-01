dea618d60e50be609582433764ecd28b9dd91da3
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2021-01-01 01:40:17 -0300

    Testando visualização dos personagens

--- src/components/CharacterList/CharacterCards/index.js
+++ src/components/CharacterList/CharacterCards/index.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-function CharacterCards() {
+function CharacterCards(props) {
   return(
-    <>
-    </>
+    <section className="character-card">
+      <h3>{props.chars.name}</h3>
+      <img src={props.chars.image} alt={props.chars.name} />
+    </section>
   );
 }
 
--- src/components/CharacterList/index.js
+++ src/components/CharacterList/index.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
-
 import api from '../../services/api';
+import CharacterCards from './CharacterCards';
 
 function CharacterList() {
   const [ CharacterList, setCharacterList ] = useState([]);
 
   useEffect(() => {
-    
-  });
-
-  api.get('/character')
+    api.get('/character')
       .then(res => {
+        console.log(res.data.results);
         setCharacterList(res.data.results);
       })
       .catch(err => {
         console.log(err);
       });
+  });
 
   return(
-    <>
-    </>
+    <section className="character-list">
+      {CharacterList.map((chars, id) => {
+          return (
+            <CharacterCards key={id} chars={chars} />
+          );
+        })
+      }
+    </section>
   );
 }
 
